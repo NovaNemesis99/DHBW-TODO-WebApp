325d5c3d952886d8a166478e2f340b57bbef1e26
Fabian Schelble <schelblefabian@gmail.com>
2020-06-21 12:41:23 +0200

    style changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/components/app-component/app.component';
 import { FooterComponent } from './app/components/footer/footer.component';
 import { NavigationComponent } from './app/components/navigation/navigation.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TasklistComponent } from './app/components/tasklist/tasklist.component';
 import { MainComponent } from './app/components/main/main.component';
 import { TaskComponent } from './app/components/task/task.component';
@@ -24,7 +23,6 @@ import { TaskEditComponent } from './app/components/task-edit/task-edit.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
