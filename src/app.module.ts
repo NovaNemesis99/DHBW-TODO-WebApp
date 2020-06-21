4a6c7dd5652e6a3f04f0ff030fbbe81b3b6011e4
Fabian Schelble <schelblefabian@gmail.com>
2020-06-21 11:34:15 +0200

    setup main app structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app/components/app-component/app.component';
 import { FooterComponent } from './app/components/footer/footer.component';
 import { NavigationComponent } from './app/components/navigation/navigation.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TasklistComponent } from './app/components/tasklist/tasklist.component';
+import { MainComponent } from './app/components/main/main.component';
+import { TaskComponent } from './app/components/task/task.component';
+import { TaskEditComponent } from './app/components/task-edit/task-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    TasklistComponent,
+    TaskComponent,
+    TaskEditComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
