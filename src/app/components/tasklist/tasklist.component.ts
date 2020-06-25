da23dc5d9e747ba016347d688269f80acbdef3c9
Fabian Schelble <schelblefabian@gmail.com>
2020-06-25 16:24:39 +0200

    basic displaying of tasklists

--- src/app/components/tasklist/tasklist.component.ts
+++ src/app/components/tasklist/tasklist.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as TodoActions from '../../store/todo.actions';
+import { Observable } from 'rxjs';
+import { Tasklist } from 'src/app/shared/models/tasklist';
+import { AppState } from 'src/app/store/app.state';
+import * as TodoSelectors from '../../store/todo.selector';
 
 @Component({
   selector: 'app-tasklist',
@@ -7,9 +13,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasklistComponent implements OnInit {
 
-  constructor() { }
+  tasklists$: Observable<Tasklist[]>;
+  isLoading$: Observable<boolean>;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new TodoActions.GetAllLists);
+    this.tasklists$ = this.store.select(TodoSelectors.selectTasklist);
   }
 
 }
