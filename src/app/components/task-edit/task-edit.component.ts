5933a0d278096a2363a8de13346fd468a1a6840c
Fabian Schelble <schelblefabian@gmail.com>
2020-06-29 14:03:31 +0200

    tasks can now be edited

--- src/app/components/task-edit/task-edit.component.ts
+++ src/app/components/task-edit/task-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, NgZone } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import * as TodoActions from '../../store/todo.actions';
+import * as TodoSelectors from '../../store/todo.selector';
+import { Observable } from 'rxjs';
+import { Task } from 'src/app/shared/models/task';
 
 @Component({
   selector: 'app-task-edit',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskEditComponent implements OnInit {
 
-  constructor() { }
+  specificTask$: Observable<Task>;
+
+  changedTask: Task = {
+    id: null,
+    name: "",
+    duedate: "",
+    description: "",
+    weight: null,
+    state: null,
+    list_id: null
+  };
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: number, private store: Store, public dialogRef: MatDialogRef<TaskEditComponent>) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new TodoActions.GetTaskById(this.data));
+    this.specificTask$ = this.store.select(TodoSelectors.selectTask);
+    this.specificTask$.subscribe(value => {
+      this.changedTask = JSON.parse(JSON.stringify(value));
+    });
   }
 
+  submitTask() {
+    this.store.dispatch(new TodoActions.AddOrUpdateTask(this.changedTask));
+    this.closeDialog();
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
--- src/app/components/tasklist-detail/tasklist-detail.component.ts
+++ src/app/components/tasklist-detail/tasklist-detail.component.ts
@@ -8,6 +8,7 @@ import { Tasklist } from 'src/app/shared/models/tasklist';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddTaskComponent } from '../add-task/add-task.component';
 import { Location } from '@angular/common';
+import { TaskEditComponent } from '../task-edit/task-edit.component';
 
 @Component({
   selector: 'app-tasklist-detail',
@@ -69,12 +70,27 @@ export class TasklistDetailComponent implements OnInit {
       this.newName = '';
       await this.delay(500);
       this.router.navigateByUrl('/tasklist', { skipLocationChange: true }).then(() => {
-        this.router.navigate(['/specificTasklist'], {fragment: this.id.toString()});
+        this.router.navigate(['/specificTasklist'], { fragment: this.id.toString() });
       });
+    } else {
+      window.alert("Zum Ändern muss ein Name eingegeben werden!")
     }
   }
 
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  changeTask(id) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = false;
+    dialogConfig.data = id;
+    dialogConfig.width = "1200px";
+
+    if (this.dialog.openDialogs.length == 0) {
+      const dialogRef1 = this.dialog.open(TaskEditComponent, dialogConfig);
+      dialogRef1.afterClosed().subscribe(() => this.getTasklist(this.id));
+    }
   }
 }
